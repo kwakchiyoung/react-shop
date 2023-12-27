13a838c2bbe7d1568caa476a4e119aec9ab4fa4f
Kwak Luca <88577086+kwakchiyoung@users.noreply.github.com>
2023-12-27 12:40:42 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Navbar, Container, Nav } from "react-bootstrap";
+import "./App.css";
+import { useState } from "react";
+// export했던 작명 그대로 써줘야함
+// import { a, b } from "./data.js";
+import data from "./data.js";
 
 function App() {
+  let [shoes] = useState(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">무신사</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="main-bg"></div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width="80%"
+            />
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price}</p>
+          </div>
+          <div className="col-md-4">
+            {/* src={process.env.PUBLIC_URL + "/logo192.png"} */}
+            <img
+              src="https://codingapple1.github.io/shop/shoes2.jpg"
+              width="80%"
+            />
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].price}</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes3.jpg"
+              width="80%"
+            />
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].price}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,32 @@
+let a = 10;
+let b = 20;
+
+let data = [
+  {
+    id: 0,
+    title: "White and Black",
+    content: "Born in France",
+    price: 120000,
+  },
+
+  {
+    id: 1,
+    title: "Red Knit",
+    content: "Born in Seoul",
+    price: 110000,
+  },
+
+  {
+    id: 2,
+    title: "Grey Yordan",
+    content: "Born in the States",
+    price: 130000,
+  },
+];
+
+//한개를 내보내고 싶을때
+// export default a;
+//여러개를 내보내고 싶을때
+// export { a, b };
+
+export default data;
